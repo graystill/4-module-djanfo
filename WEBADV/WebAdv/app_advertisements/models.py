b81916b2389c992f8191a54326d24efccaf28952
A_A_D <pilcuktatana863@gmail.com>
2023-08-10 11:17:56 +0300

    домашка 4 модуль 7 урок

--- WEBADV/WebAdv/app_advertisements/models.py
+++ WEBADV/WebAdv/app_advertisements/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Advertisement(models.Model):
+    title = models.CharField('заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'Advertisement(id={self.id}, titile={self.title}, price={self.price})'
+
+    class Meta:
+        db_table = "advertisements"
