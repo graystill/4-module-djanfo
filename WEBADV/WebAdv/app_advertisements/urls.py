db5b0ea3990486859e4b09a2aaa09b3bf28867f7
A_A_D <pilcuktatana863@gmail.com>
2023-08-26 22:43:31 +0300

    домашка 4 модуль 10 урок

--- WEBADV/WebAdv/app_advertisements/urls.py
+++ WEBADV/WebAdv/app_advertisements/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import index, top_sellers, advertisement_post, register, login, profile
 
 urlpatterns = [
-    path('', index, name='main_page'),
-    path('top_sellers/', top_sellers, name='top_sellers'),
-    path('advertisement_post/', advertisement_post, name='advertisement_post'),
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='adv-post'),
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('profile/', profile, name='profile'),
--- WEBADV/WebAdv/app_advertisements/views.py
+++ WEBADV/WebAdv/app_advertisements/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Advertisement
+from .forms import AdvertisementForm
+from django.urls import reverse
 
 
 def index(request):
@@ -10,11 +12,23 @@ def index(request):
 
 
 def top_sellers(request):
-    return render(request, 'top_sellers.html')
+    return render(request, 'top-sellers.html')
 
 
 def advertisement_post(request):
-    return render(request, 'advertisement_post')
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
 
 
 def register(request):
